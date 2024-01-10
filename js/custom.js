213c3b96152a8ee1bbe921c3c8b98959d26df474
borgesachin365 <borgesachin365@gmail.com>
2024-01-10 19:34:01 +0530

    half development done

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,39 @@
-console.log("you logged")
+//Offerings carousel slider
+$('.offerings_slider').owlCarousel({
+  center: true,
+  loop: true,
+  items: 3,
+  margin:20,
+  autoplay:false,
+//   nav:true
+});
+
+//Awards carousel slider
+$('.carousel-main').owlCarousel({
+	items: 3,
+	loop: true,
+	autoplay: true,
+	autoplayTimeout: 1500,
+	margin: 20,
+	nav: true,
+	dots: false,
+	navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'],
+})
+
+//Brands carousel slider
+$('.brand-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:3
+      },
+      1000:{
+        items:9
+      }
+    }
+  })
+
